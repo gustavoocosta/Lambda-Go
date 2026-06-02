9945ca8a4a8e4f651d0841027f892c1ea9a5b19a
gustavoocosta <gustavocosta2407@gmail.com>
2026-06-02 10:46:25 -0300

    test: adiciona execução local para Lambda

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/lambda"
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/gustavoocosta/Lambda-Go/internal/handler"
 )
 
 func main() {
-	lambda.Start(handler.Hello)
+	response, err := handler.Hello(
+		context.Background(),
+		events.APIGatewayProxyRequest{
+			QueryStringParameters: map[string]string{
+				"name": "Gustavo",
+			},
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.Body)
 }
